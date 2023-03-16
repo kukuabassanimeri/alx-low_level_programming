4a163f6f3c587bbbd82ee9c4041833e19997a8d9
ABASS KUKU <125487152+kukuabassanimeri@users.noreply.github.com>
2023-03-16 06:59:43 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/* Here is a source file known as 4-puts.c */
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return(0);
+}
+
+
+
+
