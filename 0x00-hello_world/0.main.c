f8242b3dd70eea18595b4582493e088fdbb9260c
ABASS KUKU <125487152+kukuabassanimeri@users.noreply.github.com>
2023-03-16 05:08:05 -0700

    Preprocessor

---
+++ 0x00-hello_world/0.main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/* This is my source.c file*/
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	retun(0);
+}
---
+++ 0x00-hello_world/README.md
@@ -0,0 +1,1 @@
+creating README.md  file inside 0x00-hello_world
