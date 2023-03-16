660794fd1d6ef0616e85376d93d6e934edafebfa
ABASS KUKU <125487152+kukuabassanimeri@users.noreply.github.com>
2023-03-16 07:17:02 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include< stdio.h>
+/* c programm that prints using printf */
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("with proper gramer, but the outcome is a piece of art,\n);
+	return(0);
+}
