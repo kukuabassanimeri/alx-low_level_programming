5f08c9d080c926d9dbd9a62eaf34dda5eeb01fa0
ABASS KUKU <125487152+kukuabassanimeri@users.noreply.github.com>
2023-03-17 17:39:44 -0700

    0x01. C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/* Write a program that prints all single digit */
+/*numbers of base 10 starting from 0, followed by a new line.*/
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int num;
+
+	num = 0;
+	
+	while (num < 10)
+	{
+		printf("%d", num);
+		num++;
+	}
+	printf("\n");
+	return (0);
+}
