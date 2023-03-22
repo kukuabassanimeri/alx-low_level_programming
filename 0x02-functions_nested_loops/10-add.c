f3ab9203de55e76970f16e688dc8544513bd360a
ABASS KUKU <125487152+kukuabassanimeri@users.noreply.github.com>
2023-03-22 09:27:02 -0700

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/*Write a function that adds two integers and returns the result.*/
+/**
+ * add - add two integers and return the result
+ *
+ * @y: first parameter for function
+ * @z: secomd parameter for function
+ * Return: always the sum of two digits
+ */
+int add(int y, int z)
+{
+	int a;
+
+	a = y + z;
+	return (a);
+}
