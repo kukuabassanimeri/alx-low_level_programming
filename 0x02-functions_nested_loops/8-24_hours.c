30d3180c1168683fdacdda1756a6ada224b55d82
ABASS KUKU <125487152+kukuabassanimeri@users.noreply.github.com>
2023-03-22 10:06:54 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,13 +16,13 @@ void jack_bauer(void)
 		b = 00;
 		while (b < 60)
 		{
-			 _putchar((a / 10) + '0');
-			 _putchar((a % 10) + '0');
-			 _putchar(';');
-			 _putchar((b / 10) + '0');
-			 _putchar((b % 10) + '0');
-			 _putchar('\n');
-			 b++;
+			_putchar((a / 10) + '0');
+			_putchar((a % 10) + '0');
+			_putchar(':');
+			_putchar((b / 10) + '0');
+			_putchar((b % 10) + '0');
+			_putchar('\n');
+			b++;
 		}
 		a++;
 	}
