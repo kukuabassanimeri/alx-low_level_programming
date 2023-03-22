76af610cfcd8c79e62e7318cec8e716394ab21c5
ABASS KUKU <125487152+kukuabassanimeri@users.noreply.github.com>
2023-03-22 06:19:27 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,20 @@
-#include <stdio.h>
+#include "main.h"
+/*Write a program that prints _putchar, followed by a new line.*/
 /**
- * main - a function that prits _putchar
- * Return: (0)
+ * main - Entry Point
+ *
+ * Return: returning 0 always as successful program
  */
 int main(void)
 {
-	putchar('_');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
-	putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
-
