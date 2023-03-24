9501c36a05d9bfd908b417d6d2d1c0d8405aee8d
ABASS KUKU <125487152+kukuabassanimeri@users.noreply.github.com>
2023-03-23 20:03:12 -0700

    0x04. C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/*Write a function that draws a straight line in the terminal.*/
+/**
+ * print_line - draw straight line
+ * @n: parameter that draw straight line
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	} else
+	{
+		int i;
+
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('-');
+		}
+		_putchar('\n');
+	}
+}
