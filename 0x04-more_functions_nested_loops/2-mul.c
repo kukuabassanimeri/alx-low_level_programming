460de3750a2711cdfd04470b621e5a96f055f04a
ABASS KUKU <125487152+kukuabassanimeri@users.noreply.github.com>
2023-03-23 17:47:20 -0700

    0x04. C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/*Write a function that multiplies two integers.*/
+/**
+ * mul - function that multiplies two integers
+ * @a: parameter for int a
+ * @b: parameter for int b
+ *
+ * Return: multiplication of two integers
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
