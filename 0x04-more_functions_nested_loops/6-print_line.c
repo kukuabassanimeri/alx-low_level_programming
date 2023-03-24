9a08bc442ded437bc2c65c82d1455a5d56b9f331
ABASS KUKU <125487152+kukuabassanimeri@users.noreply.github.com>
2023-03-23 20:06:58 -0700

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,7 +6,7 @@
  */
 void print_line(int n)
 {
-	if (n <= 0)
+	if (n > 0)
 	{
 		_putchar('\n');
 	} else
@@ -15,7 +15,7 @@ void print_line(int n)
 
 		for (i = 1; i <= n; i++)
 		{
-			_putchar('-');
+			_putchar('_');
 		}
 		_putchar('\n');
 	}
