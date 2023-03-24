54ca82cad4fdfa37bc603e78183023319f7843b5
ABASS KUKU <125487152+kukuabassanimeri@users.noreply.github.com>
2023-03-23 19:47:44 -0700

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,7 +2,7 @@
 /*Write a function that prints 10 times the numbers,*/
 /*from 0 to 14, followed by a new line.*/
 /**
- * more_numbers - print 10 times the numbers from 0 to 14
+ * more_numbers - print 10 times the numbers from 0 to -14
  *
  * Return: void
  */
@@ -15,11 +15,10 @@ void more_numbers(void)
 		for (j = 0; j <= 14; j++)
 		{
 			if (j >= 10)
-			{
 				_putchar('1');
-				_putchar(j % 10 + '0');
-			}
-			_putchar('\n');
+			_putchar(j % 10 + '0');
 		}
+		_putchar('\n');
+
 	}
 }
