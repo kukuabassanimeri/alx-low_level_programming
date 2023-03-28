336a8f391778731654545c903f26f25c30a8ed1c
ABASS KUKU <125487152+kukuabassanimeri@users.noreply.github.com>
2023-03-28 00:49:39 -0700

    task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/*a function that prints n elements of*/
+/*an array of integers, followed by a new line.*/
+/**
+ * print_array - prints n element
+ *@a: parameter int *a
+ * @n: parameter int n
+ *main -Entry point
+ *Return: void
+ */
+void print_array(int *a, int n)
+{
+	int j;
+
+	for (j = 0; j < n; j++)
+	{
+	printf("%d", a[j]);
+	if (j != (n - 1))
+	{
+	printf(",");
+	}
+	}
+	printf("\n");
+}
+
