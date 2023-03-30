9ef435dbf65081db21f8b11d465badeb8704558a
ABASS KUKU <125487152+kukuabassanimeri@users.noreply.github.com>
2023-03-30 04:20:27 -0700

    task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/*function that concatenates two strings.*/
+/**
+ * _strncat - function that concatenates two strings
+ * @dest: parameter
+ * @src: parameter
+ * @n: int parameter
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int length_1 = strlen(dest);
+	int length_2 = strlen(src);
+	int length_3 = n / sizeof(src[0]);
+	int index;
+	int i;
+
+	for (i = 0; i < length_3; i++)
+	{
+		if (i < length_2)
+		{
+			index = length_1 + i;
+			dest[index] = *src;
+			src++;
+		}
+		
+	}
+	return (dest);
+}
