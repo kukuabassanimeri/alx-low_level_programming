727c81c3fc9562b9445e1f8845be7f5d7e4056ab
ABASS KUKU <125487152+kukuabassanimeri@users.noreply.github.com>
2023-04-05 12:06:23 -0700

    task0

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,11 @@
+#ifndef main_h
+#define main_h
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+
+#endif
