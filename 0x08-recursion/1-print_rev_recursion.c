291f1a8a48758eafea166dfaae61a367c87bce6f
ABASS KUKU <125487152+kukuabassanimeri@users.noreply.github.com>
2023-04-05 12:10:11 -0700

    task1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/*a function that prints a string in reverse.*/
+/**
+ * _print_rev_recursion - my function
+ *
+ * @s: parameter
+ * Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
