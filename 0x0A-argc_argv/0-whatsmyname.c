8a25a974aa990795b450670221bbf0112a0ae52a
ABASS KUKU <125487152+kukuabassanimeri@users.noreply.github.com>
2023-04-09 14:46:53 -0700

    task0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/*a program that prints its name, followed by a new line.*/
+/**
+ * main - function
+ *
+ * @argc: first parameter
+ * @argv: second parameter
+ * Return: int
+ */
+int main(__attribute__((unused))int argc, char *argv[])
+{
+	int k = 0;
+
+	printf("%s\n", argv[k]);
+	return (0);
+}
