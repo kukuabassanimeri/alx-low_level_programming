baf04b0029b2b30b29ea6a9ccdab7b7916fba452
ABASS KUKU <125487152+kukuabassanimeri@users.noreply.github.com>
2023-04-20 13:40:39 -0700

    task1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -2,9 +2,10 @@
 /*Write a function that prints numbers, followed by a new line*/
 /**
  * print_numbers - function
- * @separator: parameter
- * @n: parameter
- * @...: parameter
+ * @separator: first parameter
+ * @n: last fixed parameter before ...
+ * @...: variadic arguments
+ *
  * Return: void
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
@@ -12,12 +13,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list numlist;
 	unsigned int k;
 
-	va_start(numlist, k);
+	va_start(numlist, n);
 	for (k = 0; k < n; k++)
 	{
 		printf("%d", va_arg(numlist, int));
 		if (separator != NULL && k != n - 1)
 			printf("%s", separator);
 	}
-	print("\n");
+	printf("\n");
 }
