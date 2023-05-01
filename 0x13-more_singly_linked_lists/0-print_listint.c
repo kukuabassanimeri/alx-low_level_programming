36977a95194832037d0a7b8a9af78b1c48281535
ABASS KUKU <125487152+kukuabassanimeri@users.noreply.github.com>
2023-05-01 15:47:13 -0700

    task0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/* a function that prints all the elements of a listint_t list.*/
+/**
+ * print_listint - Our function
+ * @h: parameter
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	unsigned int count = 0;
+
+	if (h == NULL)
+	{
+		printf("Linked list empty\n");
+		return (0);
+	}
+	for (; h != NULL;)
+	{
+		count++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (count);
+}
