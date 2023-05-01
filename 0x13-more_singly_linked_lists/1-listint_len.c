a33c66a44386f780b492739db10946886496a0e5
ABASS KUKU <125487152+kukuabassanimeri@users.noreply.github.com>
2023-05-01 16:33:37 -0700

    task1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/*function that returns the number of elements*/
+/*in a linked listint_t list.*/
+/**
+ * listint_len - Our function
+ * @h: parameter
+ *
+ * Return: number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	if (h == NULL)
+	{
+		printf("No nodes in the linked list ");
+		printf("using to tranverse the nodes.");
+		return (1);
+	}
+	for (; h != NULL;)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
